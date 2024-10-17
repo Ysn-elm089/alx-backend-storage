4965ac3cffd19defeedd9403b7b331de9859bd47
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2024-10-17 21:37:54 +0000

    first

---
+++ 0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Task 0: Create the `users` table
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
